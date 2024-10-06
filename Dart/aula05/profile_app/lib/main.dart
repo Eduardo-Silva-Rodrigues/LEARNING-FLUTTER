b2d6aa76239ff358e5b0abedd311123ceded3551
srduardo <eduardodev8@gmail.com>
2024-10-06 19:00:07 -0300

    Updated: Cabeçalho, Avatar, Background do Avatar e Dados do Usuário da atividade de Flutter implementados

--- Dart/aula05/profile_app/lib/main.dart
+++ Dart/aula05/profile_app/lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class ProfileApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +23,7 @@ class MyProfilePage extends StatelessWidget {
   final String nome = "Eduardo Silva Rodrigues";
   final String email = "eduardodev8@gmail.com";
   final String localizacao = "Paraná, Brasil";
-  final List<String> habilidades = <String> [
+  final List<String> habilidades = <String>[
     "Java",
     "Spring",
     "Git",
@@ -36,24 +35,90 @@ class MyProfilePage extends StatelessWidget {
     "Flutter",
     "SQL"
   ];
-
+  final String imagem =
+      "https://paraibaon.com.br/wp-content/uploads/2022/05/guaxininanjnd_widelg.jpg";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
-            title: const Text("Profile App"), 
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            titleTextStyle: const TextStyle(color: Colors.black),
-          ),
-          body: SingleChildScrollView(
-            child: Column(
+      appBar: AppBar(
+        title: const Text("Profile App"),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        titleTextStyle: const TextStyle(color: Colors.black),
+      ),
+      body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+                child: Stack(
               children: [
-                Stack(
-                  
+                Container(
+                  color: Colors.blue,
+                  height: 180,
                 ),
-          ])),
+                Positioned(
+                    top: 40,
+                    left: 150,
+                    child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://th.bing.com/th/id/OIP.0lHYSgYIxJp7iNuFdJ-9aQHaEK?w=270&h=180&c=7&r=0&o=5&pid=1.7'),
+                                fit: BoxFit.cover))))
+              ],
+            ))
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(
+              Icons.person,
+              color: Colors.blue,
+              size: 20,
+              semanticLabel: "Nome do usuário",
+            ),
+            Text(nome,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+          ],
+        ),
+        Row(
+          textDirection: TextDirection.ltr,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(
+              Icons.map,
+              color: Colors.blue,
+              size: 20,
+              semanticLabel: "Localização do usuário",
+            ),
+            Text(
+              localizacao,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(
+              Icons.email,
+              color: Colors.blue,
+              size: 20,
+              semanticLabel: "Email do usuário",
+            ),
+            Text(email,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+          ],
+        )
+      ])),
     );
   }
 }
